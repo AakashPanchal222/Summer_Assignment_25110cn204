8ed5913173c97cdf86867e8b8dfe0cd99a5dedf3
Aakash Panchal <aakashpanchal222198@gmail.com>
2026-06-08 20:25:30 +0530

    Add files via upload

---
+++ DAY 8/QUE29.java
@@ -0,0 +1,18 @@
+// *
+// **
+// ***
+// ****
+// *****
+
+public class QUE29 {
+    public static void main(String[] args) {
+        int rows = 5;
+
+        for (int i = 1; i <=rows; i++) {// for rows
+         for (int j=1; j <= i; j++) {// for coloumms
+            System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ DAY 8/QUE31.java
@@ -0,0 +1,20 @@
+//A
+//AB
+//ABC
+//ABCD
+//ABCDE
+
+public class QUE31 {
+    
+    public static void main(String[] args) {
+    int rows = 5;
+
+        for (int i= 1; i<= rows;i++) {
+        for (char ch = 'A'; ch < 'A' + i; ch++) {
+        System.out.print(ch);
+            }
+            System.out.println();
+        }
+    }
+}
+    
