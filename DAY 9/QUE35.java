7116772d263e5c2b290fa29e97a0cec820dc953a
Aakash Panchal <aakashpanchal222198@gmail.com>
2026-06-09 21:48:00 +0530

    Add files via upload

---
+++ DAY 9/QUE35.java
@@ -0,0 +1,25 @@
+package DAY9;
+//A
+//BB
+//CCC
+//DDDD
+//EEEEE
+
+public class QUE35 {
+     public static void main(String[] args) {
+        
+        int rows = 5;
+
+        for (int i = 1; i<= rows; i++) {
+           
+            char ch = (char) ('A' + i - 1);
+
+        for (int j = 1; j<= i; j++) {
+             System.out.print(ch);
+        }
+
+            System.out.println();
+    }
+ }
+}
+
